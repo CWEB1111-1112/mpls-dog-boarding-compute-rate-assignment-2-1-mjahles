ca99f179e69d02370012939adf46d1fd46f7dd7e
mjahles <ahlmatj@dunwoody.edu>
2020-04-10 15:19:46 -0500

    Created global variables stayDuration and servicesIncluded. Created the welcome method. Created prompts for stay duration and whether there will be service addons included

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace assignment2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int stayDuration;
+            string servicesIncluded;
+
+            welcome();
+            //Stay duration
+            Console.WriteLine("Please input the number of days your dog will be staying with us:");
+            stayDuration = Console.ReadLine();
+            //Service add-on yes/no
+            Console.WriteLine("Would you like any add-on services? (Y/N):");
+            servicesIncluded = Console.ReadLine();
+        }
+
+        //welcome method
+        static void welcome(){
+            Console.WriteLine("Welcome to MPLS Dog Boarding!\n");
         }
     }
 }
